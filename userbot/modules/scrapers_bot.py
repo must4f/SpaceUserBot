845bcc8e8857a7249a6783a3d6257f4f0986eaf0
whomiri <75207175+whomiri@users.noreply.github.com>
2021-01-26 14:55:41 +0400

    Update scrapers_bot.py

--- userbot/modules/scrapers_bot.py
+++ userbot/modules/scrapers_bot.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2020 Yusuf Usta.
+# Copyright (C) 2020 Miri.
 #
 # Licensed under the GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# Space UserBot - TheMiri
 
 import datetime
 from telethon import events
