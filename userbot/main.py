ece8983df132ee484675b3af8d37b245b86c03d2
Ferid Dadashzade <73734920+FaridDadashzade@users.noreply.github.com>
2021-01-27 12:21:15 +0400

    Update main.py

--- userbot/main.py
+++ userbot/main.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# Space UserBot - The Miri - Dadashzade
 
 """ UserBot başlangıç noktası """
 import importlib
