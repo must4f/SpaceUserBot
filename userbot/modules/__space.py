827e85b32d943191938b27124eef735c928af72f
Ferid Dadashzade <73734920+FaridDadashzade@users.noreply.github.com>
2021-01-27 13:08:30 +0400

    Update __space.py

--- userbot/modules/__space.py
+++ userbot/modules/__space.py
@@ -35,7 +35,7 @@ async def asena(event):
         sayfa = [sorted(list(CMD_HELP))[i:i + 5] for i in range(0, len(sorted(list(CMD_HELP))), 5)]
         
         for i in sayfa:
-            string += f'`▶️ `'
+            string += f'`👨‍🚀 `'
             for sira, a in enumerate(i):
                 string += "`" + str(a)
                 if sira == i.index(i[-1]):
